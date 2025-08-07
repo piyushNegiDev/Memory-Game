3ee07a2808af1588b2c5f74facb38966d4439599
piyushNegiDev <piyushnegi381@gmail.com>
2025-08-07 10:28:53 +0530

    Updated version but there is need to change style I'll do it in the afternoon

--- script.js
+++ script.js
@@ -35,8 +35,9 @@ function generateHTML() {
 }
 
 function flipCard(card) {
-  if (lockBoard || card === firstCard || card.classList.contains("flipped")) return;
-  
+  if (lockBoard || card === firstCard || card.classList.contains("flipped"))
+    return;
+
   startTimer();
   card.classList.add("flipped");
   card.innerHTML = card.dataset.emoji;
@@ -75,3 +76,57 @@ function resetBoard() {
 }
 
 generateHTML();
+
+// let click = 0;
+// let contentArray = [];
+
+// function cardFlip() {}
+
+// function generateHTML() {
+//   let html = ``;
+//   let grid = document.querySelector(".grid");
+
+//   let afterNine = 1;
+//   for (let index = 1; index <= 16; index++) {
+//     if (index >= 9) {
+//       html += `<div id="card${index}" class="card card${index}">${afterNine}</div>`;
+
+//       afterNine++;
+//     } else {
+//       html += `<div id="card${index}" class="card card${index}">${index}</div>`;
+//     }
+//   }
+
+//   grid.innerHTML = html;
+// }
+// generateHTML();
+
+// for (let index = 1; index <= 16; index++) {
+//   let card = document.querySelector(`.card${index}`);
+
+//   card.addEventListener("click", () => {
+//     card.style.visibility = "hidden";
+//     click++;
+
+//     contentArray.unshift(card.innerHTML);
+
+//     console.log(contentArray);
+
+//     if (click % 2 === 0) {
+//       console.log("working");
+//       compareContent();
+//     }
+//   });
+// }
+
+// function compareContent() {
+//   if (contentArray[0] === contentArray[1]) {
+//     console.log("matched");
+//     contentArray = [];
+//   } else {
+//     contentArray.forEach((content) => {
+//       document.getElementById(`card${content}`).style.visibility = "visible";
+//     });
+//     contentArray = [];
+//   }
+// }
